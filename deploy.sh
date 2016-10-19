bed61a46bfe18dbc6539745c59427ecf26dce5ad
Matt Liszewski <mliszews82@gmail.com>
2016-10-19 15:00:23 -0400

    fix push

--- deploy.sh
+++ deploy.sh
@@ -92,7 +92,7 @@ make_task_def(){
 
 push_ecr_image(){
 	eval $(aws ecr get-login --region us-east-1)
-	docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/mliszewski/service-boilerplate:$CIRCLE_SHA1
+	docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/service-boilerplate:$CIRCLE_SHA1
 }
 
 register_definition() {
